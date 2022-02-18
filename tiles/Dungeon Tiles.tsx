7bcff7c98fb64d88e1807d7e6136425876f022d2
0xbeedao <0xbeedao@users.noreply.github.com>
2022-02-18 18:58:59 +0000

    deploy: 103da98f7fe0754e4cfb4952bc879c6355c07c6a

--- tiles/Dungeon Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="Dungeon Tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../src/assets/tilesets/tilset_dungeon_32x32.png" width="512" height="512"/>
-</tileset>
--- tiles/More Dungeon.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="More Dungeon" tilewidth="32" tileheight="32" tilecount="28" columns="7">
- <image source="../src/assets/tilesets/32x32extratiles.png" width="224" height="128"/>
- <wangsets>
-  <wangset name="CurlyWalls" type="mixed" tile="-1"/>
- </wangsets>
-</tileset>
