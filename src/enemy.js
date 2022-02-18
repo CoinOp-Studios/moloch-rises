ae0ed70a26c743ff7948da75cdf0098eb7b5f656
John Knudson <jknudson@umich.edu>
2022-02-18 11:41:59 -0700

    enemy and player inherit from collidable sprite

--- src/enemy.js
+++ src/enemy.js
@@ -1,22 +1,11 @@
 import Phaser from 'phaser';
+import { CollidableSprite } from './collidableSprite';
 
 import { TILEHEIGHT, TILEWIDTH, INPUT} from './dungeonScene';
 
-export class Enemy extends Phaser.Physics.Arcade.Sprite {
+export class Enemy extends CollidableSprite {
     constructor(scene, x, y, texture, frame) {
-        super(scene, x, y, texture, 1);
-        // the top left pixel of the player is the
-        // "anchor" for its x and y coordinates, as opposed
-        // to the center
-        this.setOrigin(0, 0);
-
-        this.scene = scene;
-        this.scene.add.existing(this);
-        this.scene.physics.add.existing(this);
-        this.setCollideWorldBounds(true);
-
-        this.setX(x * TILEWIDTH);
-        this.setY(y * TILEHEIGHT);
+        super(scene, x, y, texture, frame);
     }
 
     update () {
