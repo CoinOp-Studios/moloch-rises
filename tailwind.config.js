61d1b7df0a65cd4b0cf5185458e6b939a129d19c
0xBigBee <0xbigbee@protonmail.com>
2022-02-18 17:18:08 -0700

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ['./index.html', './src/**/*.{html,js}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'raid-red': '#ff3864',
+      },
+      fontFamily: {
+        head: ['Nosifer', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
