52b3968508b1c589cfbedc4f1378d2a11a89bc2b
John Knudson <jknudson@umich.edu>
2022-02-19 22:44:31 -0700

    add rolls + attack for base character class

--- src/character.js
+++ src/character.js
@@ -32,6 +32,8 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
         this.hp = 0;
         this.ap = 0;
         this.dp = 0;
+        this.numDice = 2;
+        this.diceSides = 6;
 
         this.initStatsFromChain();
     }
@@ -51,7 +53,10 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
 
         // play sound
         this.playSound('move');
-        this.animateDialogue('generic');
+
+        if (this.vrfProvider.roll(10) == 10){
+            this.animateDialogue('generic');
+        }
     }
 
     tileX() {
@@ -62,25 +67,54 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
         return this.y / TILEHEIGHT;
     }
 
+    getName() {
+        // hacky way to uniquely determine name of the sprite
+        return this.texture.key;
+    }
+
     isDead() {
         return this.hp <= 0;
     }
 
     attack(character) {
-        // calculate damage w/ randomness
-
-        // animate attack
-
-        // animate dialogue
-
-        // play sound
-        this.playSound('attack');
-
-        // apply damage to character
         if (!character.isDead()) {
-            character.takeDamage();
+            // calculate damage w/ randomness
+            var damage = this.calculateDamage();
+
+            // animate attack
+
+            // animate dialogue
+            this.animateDialogue('attack');
+
+            // play sound
+            this.playSound('attack');
+
+            // apply damage to character
+            character.takeDamage(damage);
+        }
+    }
+
+    calculateDamage() {
+        // adapting from Simple D6 - 3rd ed: https://i.4pcdn.org/tg/1372924544491.pdf
+        // ap === num dice
+
+        // roll dice
+        var maxRoll = 0;
+        var numMax = 0;
+        for (var i = 0; i < this.ap; i++) {
+            var roll = this.vrfProvider.roll(this.diceSides);
+            if (roll > maxRoll) {
+                maxRoll = roll;
+                continue;
+            }
+            if (roll == this.diceSides) {
+                console.log("%s: natural %s added to total", this.getName(), this.diceSides);
+                ++numMax;
+            }
         }
 
+        // damage should never exceed numSides (6) + ap - 1
+        return maxRoll + numMax;
     }
 
     takeDamage(damageDealt) {
@@ -91,6 +125,7 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
 
         // apply damage
         this.hp -= damageReceived;
+        console.log("%s: takeDamage; damageReceived %s, remaining hp %s ", this.getName(), damageReceived, this.hp);
         if (this.hp <= 0) {
             this.kill();
         }
