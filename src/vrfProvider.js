8b2ce380cc6a59a6083b919dfa7034c9ef7fb17d
John Knudson <jknudson@umich.edu>
2022-02-19 18:11:40 -0700

    add class vrfProvider

---
+++ src/vrfProvider.js
@@ -0,0 +1,13 @@
+import Math from "core-js/fn/math";
+
+export class VrfProvider {
+    constructor() {
+
+    }
+
+    // i.e., rolling a 'sided' die
+    // returns a value between 1 + 'sides', inlusive
+    roll(sides) {
+        Math.floor(Math.random() * sides) + 1;
+    }
+}
