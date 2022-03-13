c6313a070c0cd744418a28f9d9bd00729255425c
Flip <13227294+Flip-Liquid@users.noreply.github.com>
2022-03-13 16:56:00 -0600

    remove override for animateDamage from enemy class

--- src/enemy.js
+++ src/enemy.js
@@ -67,8 +67,4 @@ export class Enemy extends Character {
         // change sprite
         this.setFrame(this.frame.name + 1);
     }
-
-    animateDamage(received, blocked) {
-        this.animateDamageStats(received, blocked);
-    }
 }
